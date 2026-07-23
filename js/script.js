10c790919952c601b1241bb5b77d7791e45cb907
petertorres232-byte <petertorres232@gmail.com>
2026-07-22 20:08:40 -0500

    javascript practice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 console.log("Custom script connected successfully!");
  console.log("This is a test message from your custom script.");
- 
+ const studentName = "John Doe"; //string type
+ let currentWeek =5;
+
+ console.log(studentName);
+ console.log(currentWeek);
+
+ // --- Constants (values that won't be reassigned) ---
+const navMenu = document.querySelector('#navbarNav');
+const contactForm = document.querySelector('#contactForm');
+const backToTopButton = document.querySelector('#backToTopBtn');
+
+// --- Let variables (values that change as the user interacts) ---
+let isNavOpen = false;
+let currentActiveSection = 'about';
